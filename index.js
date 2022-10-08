b574c8b228c379f0c02d9b5e252d9a943971e6ce
Alma-GH <cuni.21@yandex.ru>
2022-10-08 18:32:23 +0300

    test1

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 
+
+const http = require("http")
+const WebSocketServer = require("ws").WebSocketServer
 const express = require("express");
-const app = express();
-const product = require("./api/product");
-
-app.use(express.json({ extended: false }));
-
-app.use("/api/product", product);
+// const product = require("./api/product");
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+const app = express();
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
+
+// app.use(express.json({ extended: false }));
+//
+// app.use("/api/product", product);
+
+wss.on('connection', function connection(ws) {
+  console.log("WS: connection")
+  ws.on('message', function (message) {
+    console.log("WS: "+message)
+    ws.send("HI")
+  })
+
+})
+
+server.listen(PORT, () => console.log("Server started on port: "+PORT))
